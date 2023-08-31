1f131d359eb441e05c08bc94287b09e131da4d2e
juanse <juansee.vedia@gmail.com>
2023-09-01 08:15:40 +1200

    subiendo ecommerce-photo a git

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+
+
+//pages 
+
+import home from './pages/Home';
+import products from './pages/Products';
+import productDetails from './pages/ProductDetails';
+import search from './pages/Search';
+
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
